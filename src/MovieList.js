e45365a1ec96c1dab0f15268dd104bde37da9b0f
Jacopo Greenslade <jgreenslade@prometheusgroup.com>
2017-03-28 16:18:13 -0400

    Add and filter movies

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,11 +1,37 @@
 import {observer} from "mobx-react";
 import React, {Component} from 'react';
 
+import Movie from "./Movie";
+
 @observer
 class MovieList extends Component {
+  filter(e) {
+    this.props.store.filter = e.target.value;
+  }
+
+  addMovie(e) {
+    if (e.which === 13) {
+      this.props.store.addMovie(e.target.value);
+      e.target.value = "";
+    }
+  }
+
   render() {
+    const {filter, watchlist, filteredMovies} = this.props.store;
+    const items = filteredMovies.map((item) => {
+        return <li><Movie 
+            title={item}
+          /></li>;
+      });
+
     return (
-      <h1>{this.props.store.watchlist[0]}</h1>
+      <div>
+        <h1>Watchlist</h1>
+        <div>{filter}</div>
+        <input classname="create" onKeyPress={this.addMovie.bind(this)} placeholder="add movie"/>
+        <input classname="filter" value={filter} onChange={this.filter.bind(this)} placeholder="filter"/>
+        <ul>{items}</ul>
+      </div>
     )
   }
 }
--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
-import {autorun, observable} from "mobx";
+import {computed, autorun, observable} from "mobx";
 
 class Store {
-    @observable watchlist = ["Logan"];
+    @observable watchlist = ["Logan", "The Evil Dead", "Get Out"];
     @observable filter = "";
+    @computed get filteredMovies() {
+        const matchesFilter = new RegExp(this.filter, "i");
+        return this.watchlist.filter(movie => !this.filter || matchesFilter.test(movie));
+    }
+
+    addMovie(value) {
+        this.watchlist.push(value);
+    }
 }
 
 var store = window.store = new Store;
